0756539db14f942e02fcf760b6ff45d63820e999
Egwaldino Cassuente <egcassuente@gmail.com>
2026-05-07 01:55:00 +0100

    feat: componente LoginForm com input de telefone

---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { useLogin } from "@/hooks/use-login";
+
+
+export const LoginForm = () => {
+  const { phone, setPhone, handleSendCode } = useLogin();
+
+  return (
+    <div className="w-96 flex flex-col gap-6">
+      <div className="flex flex-col gap-1">
+        <p className="text-zinc-200 text-xl font-semibold">Entrar</p>
+        <p className="text-zinc-400 text-sm">
+          Introduz o teu número de telefone para receber um código de
+          verificação.
+        </p>
+      </div>
+
+      <FieldGroup>
+        <Field>
+          <Label htmlFor="phone">Número de telefone</Label>
+          <Input
+            id="phone"
+            name="phone"
+            placeholder="+244923456789"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </Field>
+      </FieldGroup>
+
+      <Button
+        className="w-full cursor-pointer bg-zinc-800 text-white hover:bg-zinc-700"
+        onClick={handleSendCode}
+      >
+        Enviar código
+      </Button>
+    </div>
+  );
+};
