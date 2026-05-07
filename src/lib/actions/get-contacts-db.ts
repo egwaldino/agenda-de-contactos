dd833195a7d215dd65d4aaed4f49ae436875606c
Egwaldino Cassuente <egcassuente@gmail.com>
2026-05-07 22:03:22 +0100

    feat: filtrar contactos pelo userId autenticado

--- src/lib/actions/get-contacts-db.ts
+++ src/lib/actions/get-contacts-db.ts
@@ -1,10 +1,17 @@
 "use server";
 
 import { prisma } from "@/lib/prisma-db";
+import { getUserId } from "../get-user-id";
 
 export const getContacts = async () => {
   try {
-    const contacts = await prisma.contact.findMany({});
+    const userId = await getUserId()
+
+    const contacts = await prisma.contact.findMany({
+      where: {
+        userId
+      }
+    });
 
     if (!contacts) return;
 
